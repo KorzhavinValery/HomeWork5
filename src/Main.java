96e69c62c5bfa44822cb3629960987b39c7032ee
Vel <korzhavin.vel@yandex.ru>
2023-06-29 22:06:23 +0700

    HomeWork5 task3

--- src/Main.java
+++ src/Main.java
@@ -38,7 +38,7 @@ public class Main {
     public static void task3() {
         System.out.println("Задача 3");
         int year = 2021;
-        if ((year % 4 == 0) && year % 100 != 0) {
+        if (year % 4 == 0 && year % 100 != 0) {
             System.out.println(year + " год является високосным.");
         } else if ((year % 4 == 0) && (year % 100 != 0) && (year % 400 == 0)) {
             System.out.println(year + " год является високосным.");
@@ -51,6 +51,31 @@ public class Main {
     public static void task4() {
         System.out.println("Задача 4");
         // Пишем код для задачи 4
+        /*
+        Правила доставки такие:
+Доставка в пределах 20 км занимает сутки.
+Доставка в пределах от 20 км до 60 км добавляет еще один день доставки.
+Доставка в пределах 60 км до 100 км добавляет еще одни сутки.
+Свыше 100 км доставки нет.
+То есть с каждым следующим интервалом доставки срок увеличивается на 1 день.
+
+Напишите программу, которая выдает сообщение в консоль: "Потребуется дней: " + срок доставки.
+
+Объявите целочисленную переменную deliveryDistance = 95, которая содержит дистанцию до клиента.
+         */
+        int deliveryDistance = 95;
+        boolean secondDistance = deliveryDistance > 20 && deliveryDistance <= 60;
+        boolean thirdDistance = deliveryDistance > 60 && deliveryDistance <= 100;
+        if (deliveryDistance <= 20){
+            System.out.println("Потребуется 1 день");
+
+        } else if (secondDistance) {
+            System.out.println("Потребуется 2 дня");
+        } else if (thirdDistance) {
+            System.out.println("Потребуется 3 дня");
+        } else {
+            System.out.println("Свыше 100 км доставки нет");
+        }
     }
 
     public static void task5() {
